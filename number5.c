3c96055ed6b5c7c6a61900115083f1d2ce63ef82
irina01 <32466638+irina01@users.noreply.github.com>
2018-04-27 23:40:17 +0300

    5 задание

---
+++ number5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int rotate (int num, int bit);
+int main(void)
+{
+    int number, bits;
+    int newnum;
+    puts("Введите число и смените бит:");
+    scanf("%d %d", &number, &bits);
+    newnum = rotate (number, bits);
+    printf("Перевёрнутое число %d.\r\n", newnum);
+    return 0;
+}
+
+int rotate(int num, int bit)
+{
+    int bs = 0;
+    int move;
+    int mask = 1;
+    int temp = num;
+    while(temp > 0)
+    {
+        temp >>= 1;
+        bs++;
+    }
+    
+    for(int i = 0; i < bs; i++)
+        mask = (mask << 1) | 1;
+    move = num >> (bs - bit%bs);
+    num <<= bit;
+    num &= mask;
+    num |= move;
+    return num;
+}
